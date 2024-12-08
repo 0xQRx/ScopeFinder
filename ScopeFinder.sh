b70d3e78d4f7d0f88b46d3e4de42906ffae5b3a8
0xQRx <157332395+0xQRx@users.noreply.github.com>
2024-12-07 20:22:53 -0500

    added TODO

--- ScopeFinder.sh
+++ ScopeFinder.sh
@@ -236,15 +236,17 @@ echo "Crawling subdomains with katana..."
 katana -list subdomains_to_crawl.txt -headless -no-sandbox -jc -d 3 -c 3 -p 3 -rd 1 -rl 5 -rlm 60 -headless -no-sandbox -o katana_crawled_URLS.txt -silent > /dev/null 2>&1
 
 # Sort URLs, separate with and without parameters
-
+# TODO: sort uniq
 # Extract all URLs with parameters
 grep -oP 'https?://[^\s"]+\?[^\s"]*' katana_crawled_URLS.txt >> URLs_with_params.txt
 grep -oP 'https?://[^\s"]+\?[^\s"]*' waymore_URLS.txt >> URLs_with_params.txt
 
+# TODO: sort uniq
 # Extract all URLs without parameters
 grep -oP 'https?://[^\s"]+' katana_crawled_URLS.txt | grep -v '\?' >> URLs_without_params.txt
 grep -oP 'https?://[^\s"]+' waymore_URLS.txt | grep -v '\?' >> URLs_without_params.txt
 
+# TODO: sort uniq
 #Extract all JS files
 grep '.js' waymore_URLS.txt >> JS_URL_endpoints.txt
 grep '.js' katana_crawled_URLS.txt >> JS_URL_endpoints.txt
